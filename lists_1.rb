50818a3118087721576977b710cc0e29fcf8e54c
Corey Haines <coreyhaines@gmail.com>
2014-02-07 14:34:12 -0600

    Add simple assert method for automated testing pleasantries!
    No need for a fancy testing framework to have automated tests. The core
    of everything is just a simple assert method that raises an exception if
    it doesn't pass.
    
    Even this assert method isn't the most basic. The most basis is
    something like
    
    ```
    def assert(condition, message="")
      if !condition
        raise "Failure! #{message}"
      end
    end
    ```
    
    But, the usage of this can be a bit harsh sometimes, and you rapidly
    move to having a assert(expected, actual).
    
    Another confusing part is the age-old question of "which comes first,
    'expected' or 'actual'?" I think of it this way: I assert that this (the
    expected) is equal to a constant (the actual).

--- lists_1.rb
+++ lists_1.rb
@@ -1,19 +1,19 @@
+require_relative 'tester'
 # 1. Write a function that returns the largest element in a list
 
 def largest_element(*args)
   array = []
   array.push(*args)
-  puts array.sort![-1]
+  array.sort![-1]
 end
-
-largest_element(5, 2, 9, 45, 0, 1, 23)
+assert 45, largest_element(5, 2, 9, 45, 0, 1, 23)
 
 # OR
 
 def largest_element(*args)
   array = []
   array.push(*args)
-  puts array.max
+  array.max
 end
 
-largest_element(100, 9, 34, 7, 289)
+assert 289, largest_element(100, 9, 34, 7, 289)
