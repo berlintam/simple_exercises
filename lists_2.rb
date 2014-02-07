7c734c835572b05dd9db262bee75a88e49be7151
tam <tam.eastley@gmail.com>
2014-02-07 11:39:40 +0100

    Reverse a list

---
+++ lists_2.rb
@@ -0,0 +1,9 @@
+# 2. Write a function that reverses a list, preferably in place.
+
+def reverse(*args)
+  array = []
+  puts "This is your given array: #{array.push(*args)}"
+  puts "This is your array in reverse: #{array.reverse}"
+end
+
+reverse(1,5,3,6,7,2,3)
