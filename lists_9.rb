2c04c00045981f7699a1ecaeb784cc64f7cb456b
tam <tam.eastley@gmail.com>
2014-02-11 13:27:57 +0100

    Concatenate two lists

---
+++ lists_9.rb
@@ -0,0 +1,10 @@
+# 9. Write a function that concatenates two lists.
+
+list_1 = [1,2,3,4,5]
+list_2 = [6,7,8,9,10]
+
+def cat_list(list1, list2)
+  list1.concat list2
+end
+
+puts cat_list(list_1, list_2)
